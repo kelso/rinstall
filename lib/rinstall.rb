a5ccfe2b0bc8fb49592ea5f5c47ace3a052c1360
Stefan Huska <stefan.huska@gmail.com>
2023-02-19 19:35:31 +0100

    Initial commit

---
+++ lib/rinstall.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "rinstall/version"
+
+module Rinstall
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/rinstall/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Rinstall
+  VERSION = "0.1.0"
+end
