19c77475ca3886fe318b27dc5342b15e6839bcea
Stefan Huska <stefan.huska@gmail.com>
2023-02-19 22:03:19 +0100

    Update cli.rb to include two methods

--- lib/rinstall/cli.rb
+++ lib/rinstall/cli.rb
@@ -2,9 +2,18 @@ require 'thor'
 
 module Rinstall
   class CLI < Thor
+    include Thor::Actions
+
     desc "bootstrap", "Install Bootstrap"
     def bootstrap
       puts "abcd"
+
+      create_file "config/initializers/initializer.rb", "# Add initialization content here"
+    end
+
+    desc "foobar", "Install Foobar (test)"
+    def foobar
+      puts "Foobar"
     end
   end
 end
